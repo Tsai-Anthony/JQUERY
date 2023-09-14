d0b1b11d58eeac01620466cee216149eea11f264
Anthony Tsai <116094020+Tsai-Anthony@users.noreply.github.com>
2023-09-14 15:49:48 -0400

    Update Droppable.java
    added notes

--- src/test/java/ui_stepdefinitions/Droppable.java
+++ src/test/java/ui_stepdefinitions/Droppable.java
@@ -8,14 +8,18 @@ public class Droppable {
 	@When("User clicks on Droppable")
 	public void user_clicks_on_droppable() {
 		pages.DroppablePage().droppableTab();
+		//this goes to the droppable tab
 		pages.DroppablePage().validatePageload();
+		//this validates that we are on the droppable page
 
 	}
 
 	@Then("User should click and drag the box to the desire location")
 	public void user_should_click_and_drag_the_box_to_the_desire_location() {
 		pages.DroppablePage().moveBox();
+		// this moves the box to the x/y location given
 		pages.DroppablePage().validateDroppedMessage();
+		// this validates if the message "Dropped!" appears when box is successfully dropped in the correct locaiton
 
 }
 }
